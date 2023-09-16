79509daefef7b4af94da7e6fd511e72dc443eccb
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-09-15 20:40:12 -0400

    Assignement done

---
+++ src/app/App.jsx
@@ -0,0 +1,19 @@
+import TransactionHistory from "../features/transactions/TransactionHistory";
+import Transactions from "../features/transactions/Transactions";
+
+import "./app.css";
+
+import { store } from './store.js'
+
+// TODO: Import the Redux store and provide it to this component using <Provider>.
+const App = () => {
+  return (
+    <main>
+      <h1>Bank Account</h1>
+      <Transactions />
+      <TransactionHistory />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import transactionsSlice from "../features/transactions/transactionsSlice";
+
+
+export const store = configureStore({
+    reducer: {
+        transactions: transactionsSlice,
+    },
+});
